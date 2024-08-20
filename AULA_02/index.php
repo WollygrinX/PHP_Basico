8815ab1b1d127cc40ed36501ceeb791e063f0278
Walacy Pereira <wollygrin@gmail.com>
2024-08-19 23:11:03 -0300

    Add files via upload
    Praticando PHP

---
+++ AULA_02/index.php
@@ -0,0 +1,8 @@
+<?php 
+    echo '<pre>';
+    print_r($_SERVER);
+    echo '</pre>';
+
+    //https://www.php.net/manual/pt_BR/reserved.variables.server.php//
+?>
+
---
+++ AULA_03/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    /*String*/
+    $nome = 'Walacy';
+    /*Valores inteiros*/
+    $idade = 29;
+
+    echo $idade;
+    echo '<br>';
+    echo $nome;
+    //declarando variaveis via php
+
+?>
