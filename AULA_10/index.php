517be33885e7bbfad79d5984e41c045487ad4bc8
Walacy Pereira <wollygrin@gmail.com>
2024-09-04 01:11:39 -0300

    Update index.php

---
+++ AULA_10/index.php
@@ -0,0 +1,41 @@
+<?php
+   
+    /*$nome = 'Hermes';
+    $nome2 = 'Renato';
+    */
+
+    $nome = 10;
+    $nome2 = '10';
+
+
+    //sinal igual quer dizer atribuindo um valor a variavel.
+    /*
+    if ($nome = $nome2) {
+        echo $nome2;
+    }
+    */
+
+    //verifica se é igual;
+    /*
+    if (c$nome == $nome2) {
+        echo 'verdade';
+
+    }
+    */
+
+    //verifica se é diferente
+    /*
+    if ($nome != $nome2) {
+        echo 'Diferente';
+    }*/
+    /*
+    if($nome == $nome2){
+        echo 'Verdade';
+    } */
+    if ($nome === $nome2) {
+        echo 'verdade';
+    }else {
+        echo 'Não';
+    }
+
+?>  
