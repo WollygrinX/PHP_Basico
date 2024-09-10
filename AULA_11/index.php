e9777c2692e8f89e249e369016bb1c9bcd1adb7b
Walacy Pereira <wollygrin@gmail.com>
2024-09-09 22:55:29 -0300

    Ultimas aulas e praticas finalizado

---
+++ AULA_11/index.php
@@ -0,0 +1,48 @@
+<?php
+   //Operadores matemáticos.
+    $numero1 = 10;
+    $numero2 = 11;
+
+
+    /*
+    if ($numero1 < $numero2) {
+        //Execute tal acao.
+    }else{
+       
+    }
+
+    if ($numero1 <= $numero2) {
+        echo 'Não é menor';
+    }
+
+
+    if ($numero1 > $numero2) {
+        //Execute tal acao.
+    }else{
+       
+    }
+
+    if ($numero1 >= $numero2) {
+        echo 'Não é maior';
+    }
+    */
+
+    $var1 = 'valor1';
+    $var2 = 'valor2';
+    $var3 = 'valor1';
+
+    //Operador Logico &&
+    /*
+    if (($var1 == $var2) && ($var = $var3)) {
+        echo 'verdadeiro';
+        
+    }else {
+        echo 'falso';
+    }*/
+
+    //operdador Logico || 'OU'
+    if (($var1 == $var2) || $var1 == $var3 ) {
+        echo 'verdade';
+    }
+
+?>
---
+++ AULA_12/index.php
@@ -0,0 +1,28 @@
+<?php
+//Lacos de repeticao for
+    /*
+    for ($contador=10;$contador>0;$contador--) { 
+        echo 'Olá mundo';
+        echo '<br>';
+        echo $contador;
+        echo '<hr>';
+    } 
+     */
+    //Laco de repeticao while
+   /*
+    $contador = 0;
+     while ($contador < 10) {
+        echo 'ola mundo';
+        echo '<hr>';
+        //encremento
+       $contador++; 
+     }*/
+    //lacos de repeticao do
+    $contador = 0;
+    do {
+        echo 'Olá mundo';
+        echo '<hr>';
+        $contador++;
+    } while ($contador < 10);
+    
+?>
